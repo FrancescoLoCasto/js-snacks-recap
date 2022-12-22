82ccfdde5a49aa56532e1669b973246363a7b18d
MyUsername <my@email.com>
2022-12-22 15:18:31 +0100

    Start to Creation a Snack_4 B

---
+++ Snack_4 B/js/script.js
@@ -0,0 +1,17 @@
+/** Chiedere all'utente quanti elementi vuole includere nell'array.
+Generare attraverso l'API (array/integers) un array contenente N numeri compresi tra 1 e 100.
+Stampare a schermo la somma dei numeri generati. */
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+
+      }
+
+    },
+
+
+    
+  }).mount('#app')
