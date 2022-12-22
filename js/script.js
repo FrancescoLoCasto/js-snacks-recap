df8d2b100184949eb6aa3c7c39f656ca5a0a1e33
MyUsername <my@email.com>
2022-12-22 10:17:00 +0100

    start snack

--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@
 Creare due div; uno conterrà numeri dispari di colore rosso e l'altro conterrà numeri pari in verde.
 Ad ogni click di un bottone chiedere all'API un numero, se è dispari metterlo nel blocco dispari, e se è pari in quello pari.
 */
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        oddNumbers: [],
+        evenNumbers: []
+      }
+
+    },
+
+    methods: {
+      randomNumbers(){
+         
+      }
+    }
+  }).mount('#app')
